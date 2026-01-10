fcf03e86d054e1f8c11d7962f0defacf0913182c
GRID-Editor <millsydev@zohomail.eu>
2026-01-10 07:43:23 +0000

    fix: unset auth header to allow public clone

--- get_repo.sh
+++ get_repo.sh
@@ -30,6 +30,7 @@ mkdir -p vscode
 cd vscode || { echo "'vscode' dir not found"; exit 1; }
 
 git init -q
+git config --local --unset-all http.https://github.com/.extraheader || true
 git remote add origin https://github.com/millsydotdev/GRID.git
 
 # Allow callers to specify a particular commit to checkout via the
